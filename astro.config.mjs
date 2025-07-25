b77b02bcc2229ae7860e5dc3adb3c8af84533afb
Kevin Alejandro Hernandez Carmona <84419125+EL4toCAARMINE@users.noreply.github.com>
2025-07-25 10:41:21 -0600

    Server island

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
   env: {
     schema:{
       SHOW_SEE_MORE_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-      RANDOM_NUMBER: envField.string({ context: 'client', access: 'public' })
+      RANDOM_NUMBER: envField.string({ context: 'server', access: 'public' })
     }
   }
 });
