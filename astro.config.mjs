52c5d3a4300a9a3b77eae8d4d428d468d3daad3b
Kevin Alejandro Hernandez Carmona <84419125+EL4toCAARMINE@users.noreply.github.com>
2025-09-12 14:17:18 -0600

    Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,24 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   env: {
     schema:{
       SHOW_SEE_MORE_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
       RANDOM_NUMBER: envField.string({ context: 'server', access: 'public' })
     }
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
